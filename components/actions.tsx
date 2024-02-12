1d2583ed5498f2930f86aa417c56e1717069dd8f
Sanket Bagad <alencolins@gmail.com>
2024-02-12 22:28:33 +0530

    Delete Functionality

---
+++ components/actions.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import { Link2, Trash2 } from "lucide-react";
+import { toast } from "sonner";
+import { useApiMutations } from "@/hooks/use-api-mutations";
+import { api } from "@/convex/_generated/api";
+
+interface ActionProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Action = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionProps) => {
+
+  const { mutate, pending } = useApiMutations(api.board.remove);
+
+  const onDelete = () => {
+    mutate({ id })
+      .then((res) => {
+        toast.success("Board deleted successfully");
+      })
+      .catch((err) => {
+        toast.error("Failed to delete board");
+      });
+  };
+
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => {
+        toast.success("Link copied to clipboard");
+      })
+      .catch(() => {
+        toast.error("Failed to copy link");
+      });
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy Board Link
+        </DropdownMenuItem>
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onDelete}>
+          <Trash2 className="h-4 w-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
