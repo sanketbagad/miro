8c16fad30f99a20346c9ad07e37f538350ad4255
Sanket Bagad <alencolins@gmail.com>
2024-02-17 19:49:44 +0530

    Working on shapes

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,30 +4,92 @@ import { useCallback, useState } from "react";
 import { Toolbar } from "../toolbar";
 import { Info } from "./info";
 import { Participants } from "./participants";
-import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import {
+  Camera,
+  Point,
+  CanvasMode,
+  CanvasState,
+  Color,
+  Layer,
+  LayerType,
+} from "@/types/canvas";
 import {
   useCanRedo,
   useCanUndo,
   useHistory,
   useMutation,
+  useStorage,
 } from "@/liveblocks.config";
+import { nanoid } from "nanoid";
 import { CursorsPresence } from "./cursors-presence";
 import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { LiveObject } from "@liveblocks/client";
+import { LayerPreview } from "./layer-preview";
 
 interface CanvasProps {
   boardId: string;
 }
+
+const MAX_LAYERS = 100;
 
 export const Canvas = ({ boardId }: CanvasProps) => {
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None,
   });
 
+  const layerIds = useStorage((root) => root.layerIds);
+
   const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+  const [lastUsedColor, setLastUsedColor] = useState<Color>({
+    r: 0,
+    g: 0,
+    b: 0,
+  });
 
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const insertLayer = useMutation(
+    (
+      { storage, setMyPresence },
+      layerType:
+        | LayerType.Ellipse
+        | LayerType.Rectangle
+        | LayerType.Text
+        | LayerType.Note,
+      position: Point
+    ) => {
+      const liveLayer = storage.get("layers");
+
+      if (liveLayer.size >= MAX_LAYERS) {
+        return;
+      }
+
+      const liveLayerIds = storage.get("layerIds");
+      const layerId = nanoid();
+      const layer = new LiveObject({
+        type: layerType,
+        x: position.x,
+        y: position.y,
+        width: 100,
+        height: 100,
+        fill: lastUsedColor,
+      });
+
+      liveLayerIds.push(layerId);
+      liveLayer.set(layerId, layer);
+
+      setMyPresence(
+        { selection: [layerId] },
+        {
+          addToHistory: true,
+        }
+      );
+      setCanvasState({ mode: CanvasMode.None });
+    },
+    [lastUsedColor]
+  );
 
   const onWheel = useCallback((event: React.WheelEvent) => {
     event.preventDefault();
@@ -57,6 +119,21 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     []
   );
 
+  const onPointerUp = useMutation(
+    ({}, e) => {
+      const point = pointerEventToCanvasPoint(e, camera);
+
+      if (canvasState.mode === CanvasMode.Inserting) {
+        insertLayer(canvasState.layerType, point);
+      } else {
+        setCanvasState({ mode: CanvasMode.None });
+      }
+
+      history.resume();
+    },
+    [camera, canvasState, insertLayer, history]
+  );
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
@@ -70,11 +147,25 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         redo={history.redo}
       />
       <svg
-      onWheel={onWheel}
-      onPointerMove={onPointerMove}
-      onPointerLeave={onPointerLeave}
-      className="h-[100vh] w-[100vh]">
-        <g>
+        onWheel={onWheel}
+        onPointerUp={onPointerUp}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+        className="h-[100vh] w-[100vh]"
+      >
+        <g
+          style={{
+            transform: `translate(${camera.x}px, ${camera.y}px)`,
+          }}
+        >
+          {layerIds.map((layerId) => (
+            <LayerPreview
+              key={layerId}
+              id={layerId}
+              onLayerPointerDown={() => {}}
+              selectionColor="black"
+            />
+          ))}
           <CursorsPresence />
         </g>
       </svg>
---
+++ app/board/[boardId]/_components/rectangle.tsx
@@ -0,0 +1,34 @@
+import { RectangleLayer } from "@/types/canvas";
+
+interface RectangleProps {
+  id: string;
+  layer: RectangleLayer;
+  onLayerPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Rectangle = ({
+  id,
+  layer,
+  onLayerPointerDown,
+  selectionColor,
+}: RectangleProps) => {
+  const { x, y, width, height, fill } = layer;
+
+  return (
+    <rect
+      className="drop-shadow-md"
+      onPointerDown={(e) => onLayerPointerDown(e, id)}
+      style={{
+        transform: `translate(${x}px, ${y}px)`,
+      }}
+      x={0}
+      y={0}
+      width={width}
+      height={height}
+      strokeWidth={1}
+      fill="#000"
+      stroke="transparent"
+    />
+  );
+};
