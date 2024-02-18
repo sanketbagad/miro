e2ad0438b47b6f402b068a62c70609d99579a1ba
Sanket Bagad <alencolins@gmail.com>
2024-02-18 16:25:06 +0530

    basic Functionalities almost done

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -27,12 +27,14 @@ import { nanoid } from "nanoid";
 import { CursorsPresence } from "./cursors-presence";
 import {
   connectionIdToColor,
+  findIntersectingLayersWithRectangle,
   pointerEventToCanvasPoint,
   resizeBounds,
 } from "@/lib/utils";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 interface CanvasProps {
   boardId: string;
@@ -99,9 +101,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     [lastUsedColor]
   );
 
-  const unselectLayers = useMutation((
-    { self, setMyPresence }
-  ) => {
+  const unselectLayers = useMutation(({ self, setMyPresence }) => {
     if (self.presence.selection.length > 0) {
       setMyPresence({ selection: [] }, { addToHistory: true });
     }
@@ -129,7 +129,28 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     [canvasState]
   );
 
- 
+  const updateSelectionNet = useMutation(
+    ({ storage, setMyPresence }, current: Point, origin: Point) => {
+      const layers = storage.get("layers").toImmutable();
+      setCanvasState({ mode: CanvasMode.SelectionNet, origin, current });
+
+      const ids = findIntersectingLayersWithRectangle(
+        layerIds,
+        layers,
+        origin,
+        current
+      );
+
+      setMyPresence({ selection: ids });
+    },
+    [layerIds]
+  );
+
+  const startMultiSelection = useCallback((current: Point, origin: Point) => {
+    if (Math.abs(current.x - origin.x) + Math.abs(current.y - origin.y) > 5) {
+      setCanvasState({ mode: CanvasMode.SelectionNet, origin, current });
+    }
+  }, []);
 
   const translateSelectedLayers = useMutation(
     ({ storage, self }, point: Point) => {
@@ -159,7 +180,6 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   );
 
   const onWheel = useCallback((event: React.WheelEvent) => {
-    event.preventDefault();
     setCamera((prev) => ({
       x: prev.x - event.deltaX,
       y: prev.y - event.deltaY,
@@ -172,12 +192,11 @@ export const Canvas = ({ boardId }: CanvasProps) => {
 
       const current = pointerEventToCanvasPoint(e, camera);
 
-      // if (canvasState.mode === CanvasMode.Pressing) {
-      //   startMultiSelection(current, canvasState.origin);
-      // } else if (canvasState.mode === CanvasMode.SelectionNet) {
-      //   updateSelectionNet(current, canvasState.origin);
-      // }
-      if (canvasState.mode === CanvasMode.Translating) {
+      if (canvasState.mode === CanvasMode.Pressing) {
+        startMultiSelection(current, canvasState.origin);
+      } else if (canvasState.mode === CanvasMode.SelectionNet) {
+        updateSelectionNet(current, canvasState.origin);
+      } else if (canvasState.mode === CanvasMode.Translating) {
         translateSelectedLayers(current);
       } else if (canvasState.mode === CanvasMode.Resizing) {
         resizeSelectedLayer(current);
@@ -299,6 +318,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <SelectionTools camera={camera} setLastUsedColor={setLastUsedColor} />
       <svg
         onWheel={onWheel}
         onPointerUp={onPointerUp}
@@ -321,6 +341,15 @@ export const Canvas = ({ boardId }: CanvasProps) => {
             />
           ))}
           <SelectionBox onResizeHandlePointerDown={onResizeHandlePointerDown} />
+          {canvasState.mode === CanvasMode.SelectionNet && canvasState.current != null && (
+            <rect
+            className="fill-blue-500/5 stroke-blue-500 stroke-1"
+            x={Math.min(canvasState.origin.x, canvasState.current.x)}
+            y={Math.min(canvasState.origin.y, canvasState.current.y)}
+            width={Math.abs(canvasState.origin.x - canvasState.current.x)}
+            height={Math.abs(canvasState.origin.y - canvasState.current.y)}
+          />
+          )}
           <CursorsPresence />
         </g>
       </svg>
